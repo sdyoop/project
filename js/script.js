d951621453f17446401ff74e6330f89cacd3b3dc
sdyoop <sdyoop@naver.com>
2023-12-19 18:14:30 +0900

    first version

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var swiper = new Swiper(".mySwiper", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+function openDs(evt, dsName) {
+  var i, dstabcontent, dstablinks;
+  dstabcontent = document.getElementsByClassName("dstabcontent");
+  for (i = 0; i < dstabcontent.length; i++) {
+    dstabcontent[i].style.display = "none";
+  }
+  dstablinks = document.getElementsByClassName("dstablinks");
+  for (i = 0; i < dstablinks.length; i++) {
+    dstablinks[i].className = dstablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(dsName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+// project//
+function openPj(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("htabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("htablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
