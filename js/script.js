00e1abcf063c8f5fccf6adc66142a41eee9cb380
sdyoop <sdyoop@naver.com>
2023-12-20 12:26:34 +0900

    second

--- js/script.js
+++ js/script.js
@@ -42,3 +42,26 @@ function openPj(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+(function( $ ) {
+  "use strict";
+  $(function() {
+      function animated_contents() {
+          $(".zt-skill-bar > div ").each(function (i) {
+              var $this  = $(this),
+                  skills = $this.data('width');
+
+              $this.css({'width' : skills + '%'});
+
+          });
+      }
+      
+      if(jQuery().appear) {
+          $('.zt-skill-bar').appear().on('appear', function() {
+              animated_contents();
+          });
+      } else {
+          animated_contents();
+      }
+  });
+}(jQuery));
